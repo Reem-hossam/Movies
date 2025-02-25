45fb25eaffec7a62b40332dd59303e9e8d5c5a23
khushua <khushuawael18@gmail.com>
2025-02-25 22:04:55 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	android/app/src/main/res/drawable/background.png
    #	lib/home/onboarding/onboarding_page.dart
    #	lib/home/onboarding/onboarding_screen.dart

--- lib/home/onboarding/onboarding_page.dart
+++ lib/home/onboarding/onboarding_page.dart
@@ -2,16 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:movies_app/theme/app%20theme.dart';
 
 class OnboardingPage extends StatelessWidget {
+  final String image, title, buttonText;
+  final String description;
   final VoidCallback? onNextButton, onBackButton;
+  final bool isLast, onboardingOne, firstScreen;
 
   const OnboardingPage(
       {super.key,
-      required this.image,
-      required this.title,
-      required this.description,
-      required this.buttonText,
-      required this.isLast,
-      this.onNextButton,
+        required this.image,
+        required this.title,
+        required this.description,
+        required this.buttonText,
+        required this.isLast,
+        this.onNextButton,
+        this.onBackButton,
+        required this.onboardingOne,
+        required this.firstScreen});
 
   @override
   Widget build(BuildContext context) {
@@ -23,9 +29,15 @@ class OnboardingPage extends StatelessWidget {
           fit: BoxFit.fill,
           height: double.infinity,
         ),
+        IntrinsicHeight(
+          child: Container(
+            padding: EdgeInsets.all(16),
             width: double.infinity,
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40)),
+                color: !firstScreen?AppTheme().background:Colors.transparent),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -34,8 +46,23 @@ class OnboardingPage extends StatelessWidget {
                     padding: EdgeInsets.all(16),
                     child: Text(title,
                         textAlign: TextAlign.center,
+                        style: firstScreen?TextStyle(
+                          fontSize: 36,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ):TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
+                        ))),
                 const SizedBox(height: 10),
+                Text(description,
                     textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                      color: firstScreen?Color.fromRGBO(255, 255, 255, 0.6):Colors.white,
+                    )),
                 // const SizedBox(height: 30),
                 Padding(
                   padding: EdgeInsets.all(16),
@@ -46,6 +73,8 @@ class OnboardingPage extends StatelessWidget {
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(15),
                           ),
+                          fixedSize: Size(
+                              MediaQuery.of(context).size.width * 0.8,
                               MediaQuery.of(context).size.height * 0.05)),
                       onPressed: onNextButton,
                       child: Padding(
@@ -55,23 +84,32 @@ class OnboardingPage extends StatelessWidget {
                         ),
                       )),
                 ),
-                        padding: const EdgeInsets.only(bottom: 16),
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.transparent,
-                              foregroundColor: Theme.of(context).primaryColor,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15),
-                              ),
-                                  MediaQuery.of(context).size.height * 0.05)),
-                          onPressed: onBackButton,
-                          child: Text(
-                            "Back",
-                          ),
+                !onboardingOne&&!firstScreen
+                    ? Padding(
+                  padding: const EdgeInsets.only(bottom: 16),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        foregroundColor: Theme.of(context).primaryColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
                         ),
-                        height: 0,
-                      ),
+                        side: BorderSide(
+                            color: Theme.of(context).primaryColor),
+                        fixedSize: Size(
+                            MediaQuery.of(context).size.width * 0.8,
+                            MediaQuery.of(context).size.height * 0.05)),
+                    onPressed: onBackButton,
+                    child: Text(
+                      "Back",
+                    ),
+                  ),
+                )
+                    : SizedBox(
+                  height: 0,
+                ),
               ],
+            ),
           ),
         ),
       ],
--- lib/home/onboarding/onboarding_screen.dart
+++ lib/home/onboarding/onboarding_screen.dart
@@ -13,35 +13,49 @@ class OnboardingScreen extends StatefulWidget {
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
   int _currentIndex = 0;
+  final CarouselSliderController _carouselController =
+  CarouselSliderController();
 
   final List<Map<String, String>> onboardingData = [
     {
       "image": "assets/images/movies_posters.png",
       "title": "Find Your Next Favorite Movie Here",
+      "description":
+      " Get access to a huge library of movies to suit all tastes. You will surely like it.",
+      "button": "Explore Now"
     },
     {
       "image": "assets/images/onboarding_1.png",
       "title": "Discover Movies",
+      "description":
+      "Explore a vast collection of movies in all qualities and genres. Find your next favorite film with ease.",
       "button": "Next"
     },
     {
       "image": "assets/images/onboarding_2.png",
       "title": "Explore All Genres",
+      "description":
+      "Discover movies from every genre, in all available qualities. Find something new and exciting to watch every day.",
       "button": "Next"
     },
     {
       "image": "assets/images/onboarding_3.png",
       "title": "Create Watchlists",
+      "description":
+      "Save movies to your watchlist to keep track of what you want to watch next. Enjoy films in various qualities and genres.",
       "button": "Next"
     },
     {
       "image": "assets/images/onboarding_5.png",
       "title": "Rate, Review, and Learn",
+      "description":
+      "Share your thoughts on the movies you've watched. Dive deep into film details and help others discover great movies with your reviews.",
       "button": "Next"
     },
     {
       "image": "assets/images/onboarding_4.png",
       "title": "Start Watching Now",
+      "description": "",
       "button": "Finish"
     },
   ];
@@ -66,6 +80,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
               },
             ),
             items: onboardingData.map((item) {
+              return OnboardingPage(
                 image: item["image"]!,
                 title: item["title"]!,
                 description: item["description"]!,
@@ -74,6 +89,8 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                   if (_currentIndex < onboardingData.length - 1) {
                     _carouselController.nextPage();
                   } else {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()));
                   }
                 },
                 onBackButton: () {
@@ -81,6 +98,10 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                     _carouselController.previousPage();
                   }
                 },
+                isLast: _currentIndex == onboardingData.length - 1,
+                onboardingOne: true ? _currentIndex == 1 : false,
+                firstScreen: true ? _currentIndex == 0 : false,
+              );
             }).toList(),
           ),
         ],
