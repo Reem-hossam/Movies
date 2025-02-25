026db1f04cdef661d3dfbfb0f181ae6472bbf246
khushua <khushuawael18@gmail.com>
2025-02-25 21:28:13 +0200

    onboarding Done

--- lib/home/onboarding/onboarding_page.dart
+++ lib/home/onboarding/onboarding_page.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:movies_app/theme/app%20theme.dart';
 
 class OnboardingPage extends StatelessWidget {
-  final String image, title, description, buttonText;
+  final String image, title, buttonText;
+  final String description;
   final VoidCallback? onNextButton, onBackButton;
-  final bool isLast, firstScreen;
+  final bool isLast, onboardingOne, firstScreen;
 
   const OnboardingPage(
       {super.key,
@@ -13,9 +14,10 @@ class OnboardingPage extends StatelessWidget {
       required this.description,
       required this.buttonText,
       required this.isLast,
-
-       this.onNextButton,
-       this.onBackButton, required this.firstScreen});
+      this.onNextButton,
+      this.onBackButton,
+      required this.onboardingOne,
+      required this.firstScreen});
 
   @override
   Widget build(BuildContext context) {
@@ -27,70 +29,87 @@ class OnboardingPage extends StatelessWidget {
           fit: BoxFit.fill,
           height: double.infinity,
         ),
-        Container(
-          height: MediaQuery.of(context).size.height * 0.33,
-          width: double.infinity,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40), topRight: Radius.circular(40)),
-              color: AppTheme().background),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Padding(
-                  padding: EdgeInsets.all(16),
-                  child:Text(title,
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.titleLarge)),
-              const SizedBox(height: 10),
-              Text(
-                description,
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-              // const SizedBox(height: 30),
-              Padding(
-                padding: EdgeInsets.all(16),
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Theme.of(context).primaryColor,
-                        foregroundColor: Colors.black,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        fixedSize: Size(MediaQuery.of(context).size.width * 0.8,
-                            MediaQuery.of(context).size.height * 0.05)),
-                    onPressed: onNextButton,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: Text(
-                        buttonText,
-                      ),
+        IntrinsicHeight(
+          child: Container(
+            padding: EdgeInsets.all(16),
+            width: double.infinity,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40)),
+                color: !firstScreen?AppTheme().background:Colors.transparent),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(title,
+                        textAlign: TextAlign.center,
+                        style: firstScreen?TextStyle(
+                          fontSize: 36,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ):TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w700,
+                          color: Colors.white,
+                        ))),
+                const SizedBox(height: 10),
+                Text(description,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                      color: firstScreen?Color.fromRGBO(255, 255, 255, 0.6):Colors.white,
                     )),
-              ),
-              !firstScreen ? Padding(
-                padding: const EdgeInsets.only(bottom: 16),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.transparent,
-                      foregroundColor: Theme.of(context).primaryColor,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      side: BorderSide(color: Theme.of(context).primaryColor),
-                      fixedSize: Size(MediaQuery.of(context).size.width * 0.8,
-                          MediaQuery.of(context).size.height * 0.05)),
-                  onPressed: onBackButton,
-                  child: Text(
-                    "Back",
-                  ),
+                // const SizedBox(height: 30),
+                Padding(
+                  padding: EdgeInsets.all(16),
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Theme.of(context).primaryColor,
+                          foregroundColor: Colors.black,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          fixedSize: Size(
+                              MediaQuery.of(context).size.width * 0.8,
+                              MediaQuery.of(context).size.height * 0.05)),
+                      onPressed: onNextButton,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Text(
+                          buttonText,
+                        ),
+                      )),
                 ),
-              ):SizedBox(
-                height: 0,
-              ),
-
-            ],
+                !onboardingOne&&!firstScreen
+                    ? Padding(
+                        padding: const EdgeInsets.only(bottom: 16),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.transparent,
+                              foregroundColor: Theme.of(context).primaryColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              side: BorderSide(
+                                  color: Theme.of(context).primaryColor),
+                              fixedSize: Size(
+                                  MediaQuery.of(context).size.width * 0.8,
+                                  MediaQuery.of(context).size.height * 0.05)),
+                          onPressed: onBackButton,
+                          child: Text(
+                            "Back",
+                          ),
+                        ),
+                      )
+                    : SizedBox(
+                        height: 0,
+                      ),
+              ],
+            ),
           ),
         ),
       ],
--- lib/home/onboarding/onboarding_screen.dart
+++ lib/home/onboarding/onboarding_screen.dart
@@ -5,54 +5,57 @@ import 'package:movies_app/home/home.dart';
 
 import 'onboarding_page.dart';
 
-
-
 class OnboardingScreen extends StatefulWidget {
-  static const String routeName="/";
+  static const String routeName = "/";
   @override
   _OnboardingScreenState createState() => _OnboardingScreenState();
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
   int _currentIndex = 0;
-  final CarouselSliderController _carouselController = CarouselSliderController();
+  final CarouselSliderController _carouselController =
+      CarouselSliderController();
 
   final List<Map<String, String>> onboardingData = [
     {
       "image": "assets/images/movies_posters.png",
       "title": "Find Your Next Favorite Movie Here",
-      "description": "Get access to a huge library of movies to suit all tastes. You will surely like it.",
-      "button": "Explore Now",
-
+      "description":
+          " Get access to a huge library of movies to suit all tastes. You will surely like it.",
+      "button": "Explore Now"
     },
     {
       "image": "assets/images/onboarding_1.png",
       "title": "Discover Movies",
-      "description": "Explore a vast collection of movies in all qualities and genres. Find your next favorite film with ease.",
+      "description":
+          "Explore a vast collection of movies in all qualities and genres. Find your next favorite film with ease.",
       "button": "Next"
     },
     {
       "image": "assets/images/onboarding_2.png",
       "title": "Explore All Genres",
-      "description": "Choose movies from every genre, be it action, adventure, drama, or sci-fi.",
+      "description":
+          "Discover movies from every genre, in all available qualities. Find something new and exciting to watch every day.",
       "button": "Next"
     },
     {
       "image": "assets/images/onboarding_3.png",
       "title": "Create Watchlists",
-      "description": "Save your favorite movies to a watchlist and enjoy them anytime.",
+      "description":
+          "Save movies to your watchlist to keep track of what you want to watch next. Enjoy films in various qualities and genres.",
       "button": "Next"
     },
     {
       "image": "assets/images/onboarding_5.png",
       "title": "Rate, Review, and Learn",
-      "description": "Share your thoughts on the movies you've watched. Dive deep into film details and help others discover great movies with your reviews.",
+      "description":
+          "Share your thoughts on the movies you've watched. Dive deep into film details and help others discover great movies with your reviews.",
       "button": "Next"
     },
     {
       "image": "assets/images/onboarding_4.png",
       "title": "Start Watching Now",
-      "description": "Enjoy top-rated movies from the comfort of your home.",
+      "description": "",
       "button": "Finish"
     },
   ];
@@ -64,46 +67,43 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
       body: Stack(
         children: [
           CarouselSlider(
-              carouselController: _carouselController,
-              options: CarouselOptions(
-                height:  MediaQuery.of(context).size.height*1,
-                autoPlay: false,
-                enableInfiniteScroll: false,
-                viewportFraction: 1,
-                onPageChanged: (index, reason) {
-                  setState(() {
-                    _currentIndex = index;
-                  });
-                },
-              ),
-              items: onboardingData.map((item) {
-                return
-                    OnboardingPage(
-
-                      image: item["image"]!,
-                      title: item["title"]!,
-                      description: item["description"]!,
-                      buttonText: item["button"]!,
-                      onNextButton: () {
-                        if (_currentIndex < onboardingData.length - 1) {
-                          _carouselController.nextPage();
-                        } else {
-                          Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginScreen()));
-                        }
-                      },
-                      onBackButton: () {
-                        if (_currentIndex < onboardingData.length ) {
-                          _carouselController.previousPage();
-                        }
-                      },
-                      isLast: _currentIndex == onboardingData.length - 1, firstScreen:true ?_currentIndex==1||_currentIndex==0:false,
-                    )
-
-
-                ;
-              }).toList(),
+            carouselController: _carouselController,
+            options: CarouselOptions(
+              height: MediaQuery.of(context).size.height * 1,
+              autoPlay: false,
+              enableInfiniteScroll: false,
+              viewportFraction: 1,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
             ),
-
+            items: onboardingData.map((item) {
+              return OnboardingPage(
+                image: item["image"]!,
+                title: item["title"]!,
+                description: item["description"]!,
+                buttonText: item["button"]!,
+                onNextButton: () {
+                  if (_currentIndex < onboardingData.length - 1) {
+                    _carouselController.nextPage();
+                  } else {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()));
+                  }
+                },
+                onBackButton: () {
+                  if (_currentIndex < onboardingData.length) {
+                    _carouselController.previousPage();
+                  }
+                },
+                isLast: _currentIndex == onboardingData.length - 1,
+                onboardingOne: true ? _currentIndex == 1 : false,
+                firstScreen: true ? _currentIndex == 0 : false,
+              );
+            }).toList(),
+          ),
         ],
       ),
     );
